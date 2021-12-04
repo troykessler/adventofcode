5a016be3a3c6371d1e565383391d10064f36c3b7
Troy Kessler <troy.kessler99@gmail.com>
2021-12-04 13:41:40 +0100

    3.2

--- 3.2/index.js
+++ 3.2/index.js
@@ -6,28 +6,61 @@ fs.readFile("./3.1/file.txt", (err, data) => {
   const binaryArray = data.toString().split("\n");
   const binaryLength = binaryArray[0].length;
 
-  let gamma = "";
-  let epsilon = "";
+  let oxygen = "";
+  let co2 = "";
 
-  let gammaDec = 0;
-  let epsilonDec = 0;
+  let oxygenDec = 0;
+  let co2Dec = 0;
 
-  for (let i = 0; i < binaryLength; i++) {
-    const zeros = binaryArray.filter((b) => b[i] === "0").length;
+  let oxygenArray = [...binaryArray];
+  let co2Array = [...binaryArray];
 
-    gamma += zeros * 2 > binaryLength ? "0" : "1";
-    epsilon += zeros * 2 > binaryLength ? "1" : "0";
+  while (oxygenArray.length > 1) {
+    for (let i = 0; i < binaryLength; i++) {
+      const zeros = oxygenArray.filter((b) => b[i] === "0").length;
+      const ones = oxygenArray.filter((b) => b[i] === "1").length;
+
+      if (zeros > ones) {
+        oxygenArray = oxygenArray.filter((b) => b[i] === "0");
+      } else {
+        oxygenArray = oxygenArray.filter((b) => b[i] === "1");
+      }
+
+      if (oxygenArray.length === 1) {
+        break;
+      }
+    }
   }
 
-  for (let i = 0; i < binaryLength; i++) {
-    if (gamma[i] === "1") {
-      gammaDec += 2 ** (binaryLength - i - 1);
-    }
+  while (co2Array.length > 1) {
+    for (let i = 0; i < binaryLength; i++) {
+      const zeros = co2Array.filter((b) => b[i] === "0").length;
+      const ones = co2Array.filter((b) => b[i] === "1").length;
 
-    if (epsilon[i] === "1") {
-      epsilonDec += 2 ** (binaryLength - i - 1);
+      if (zeros > ones) {
+        co2Array = co2Array.filter((b) => b[i] === "1");
+      } else {
+        co2Array = co2Array.filter((b) => b[i] === "0");
+      }
+
+      if (co2Array.length === 1) {
+        break;
+      }
     }
   }
 
-  console.log(gammaDec * epsilonDec);
+  oxygen = oxygenArray[0];
+  co2 = co2Array[0];
+
+  for (let i = 0; i < binaryLength; i++) {
+    if (oxygen[i] === "1") {
+      oxygenDec += 2 ** (binaryLength - i - 1);
+    }
+
+    if (co2[i] === "1") {
+      co2Dec += 2 ** (binaryLength - i - 1);
+    }
+  }
+
+  console.log(oxygenDec * co2Dec);
 });
